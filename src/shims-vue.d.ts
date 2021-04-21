1e712f9a6d2974ae087c4b33dd86d23e0903c3d4
unknown <471535104@163.com>
2021-04-21 17:08:25 +0800

    add note

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module '@ckeditor/ckeditor5-vue';
+declare module '@ckeditor/ckeditor5-build-decoupled-document';
